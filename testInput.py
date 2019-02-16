219c5e24915dcec40a8c28e9ce969faaa769abdb
Joshua Tavassolikhah <joshtavassolikhah@yahoo.com>
2019-02-16 01:15:25 -0800

    testinput file

--- testInput.py
+++ testInput.py
@@ -1,2 +1,17 @@
 import csv
+import json
+
+query = 'json object'
+
+radius = '1 mile'
+
+result = 'json file of longs and lats and content description'
+
+vehicle_info = open('vehicles.csv', mode='r')
+reader = csv.reader(vehicle_info)
+#query_to_dict = json.loads(query)
+
+
+def find_relevant_incidents(lng, lat, radius):
+    pass
 
