e67a06ccd1c4dc2e710a30c6da3f44265ce36f99
sephmedina <joseph.dr.medina@gmail.com>
2019-02-16 14:32:56 -0800

    Adding night mode

--- static/js/functions.js
+++ static/js/functions.js
@@ -10,6 +10,9 @@ var circle;
 function initMap(){
     //remove clutter on map
     var mapStyles = [
+        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
         {
             featureType: 'poi',
             stylers: [{visibility: 'off'}]
@@ -17,7 +20,68 @@ function initMap(){
         {
             featureType: 'transit',
             stylers: [{visibility: "off"}]
-        }
+        },
+
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
     ];
     var options = {
         zoom: 13,
@@ -72,15 +136,16 @@ function initMap(){
         deleteMarkers(null);
     };
     // set SF boundaries;
-
     var SFboundaries;
     var input = document.getElementById('input');
 
+    //set up autocomplete box
     var field = new google.maps.places.Autocomplete(input);
     field.bindTo('bounds', map);
     field.setFields(['geometry', 'name']);
     map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
 
+    //main area of functionality
     field.addListener("place_changed", function() {
         result = field.getPlace();
         if (!result.geometry) {
@@ -96,22 +161,25 @@ function initMap(){
             //map = new google.maps.Map(document.getElementById('map'), options);
             console.log(result);
             addMarker(p);
+            input.value = "";
 
             //create circle at this location
-            delete circle;
-            circle = new google.maps.Circle(
-                {   center: p.coords,
-                    strokeColor: 'blue',
-                    strokeOpacity: 0.5,
-                    strokeWeight: 1,
-                    fillColor: 'blue',
-                    editable: false,
-                    draggable: false,
-                    fillOpacity: 0.3,
-                    map: map,
-                    radius: 100
-                }
-            );
+            if (circle != null)
+                circle.setCenter(p.coords);
+            else
+                circle = new google.maps.Circle(
+                    {   center: p.coords,
+                        strokeColor: 'blue',
+                        strokeOpacity: 0.5,
+                        strokeWeight: 1,
+                        fillColor: 'blue',
+                        editable: false,
+                        draggable: false,
+                        fillOpacity: 0.3,
+                        map: map,
+                        radius: 100
+                    }
+                );
             //set up JSON for flask to handle
             var location = result.geometry.location.toJSON();
             console.log("location: ", location);
@@ -125,18 +193,14 @@ function initMap(){
                 contentType: "application/json",
                 dataType: "json"
             });
-            input.value = "";
+            //get results from url - results
+            // <% for r in {{results}} %}
+            //     p = new Props(r.latitude, r.longitude, r.description);
+            //     addMarker(p);
+            // {% endfor %}
             var results = $.get("/results");
             console.log("results: ", results);
-            //get multiple JSON strings
-            //create markers for each
         }
-
-        //TODO search for vehicle incidents - backend
-        //TODO back end code
     });
-    function createCircle() {
-        //delete existing circle
-        //create one at current map location
-    }
+
 }
