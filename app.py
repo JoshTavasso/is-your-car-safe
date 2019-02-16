08026c11c8e8d9c0005d7ae352c787e63da3704e
sephmedina <joseph.dr.medina@gmail.com>
2019-02-16 11:06:30 -0800

    trying to solve 500 errors

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ def index():
 	"""Landing Page"""
 	return render_template('map.html')
 
-@app.route('/results', methods = ['POST'])
+@app.route('/results', methods = ['POST', 'GET'])
 def get_lat_long(jsdata):
 	jsdata = request.form['location']
 	with open(jsdata, 'r') as file:
 		coordinate_dict = json.load(file)
-	return GatherIncidents.create_relevant_incidents(coordinate_dict["lng"], coordinate_dict["lat"], 1) 
+	return GatherIncidents.create_relevant_incidents(coordinate_dict["lng"], coordinate_dict["lat"], 1)
 
 # running server/application
 if __name__ == '__main__':
